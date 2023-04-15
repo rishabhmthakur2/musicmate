e5dc498f19145cda7c8e0b5e169f70d97c8e14cc
trex7 <akdhondi@gmail.com>
2023-04-15 00:47:10 -0700

    adding api to fetch all users for chat

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -28,6 +28,31 @@ router.get("/:id", async (request, response) => {
 
 });
 
+// get all the users this person is chatting with
+router.get("/chatlist/:id", async (request, response) => {
+  var tempid = request.params.id
+
+  if (tempid.length != 24) {
+    response.status(400).send("Please send a valid id of 24 characters")
+  } else {
+
+    try {
+      // const output = await Message.find({ sender_id: request.params.id });
+      const result = await Message.distinct("receiver_id", { sender_id: tempid });
+      console.log(result);
+      try {
+        response.status(200).send(result);
+      } catch (error) {
+        response.status(500).send(error);
+      }
+    } catch (error) {
+      response.status(500).send(error);
+    }
+  }
+
+
+});
+
 // // GET listing for all users.
 // router.get('/', function(req, res) {
 //   User.find({}, function(err, users) {
