1e0a52acb85bb0131d4b5f1543b42a412590d6e4
avashadhikari <avash.adhikari@berkeley.edu>
2023-05-03 17:01:13 -0700

    improvements for search

--- frontend/src/app/components/ListGroup/index.jsx
+++ frontend/src/app/components/ListGroup/index.jsx
@@ -19,73 +19,75 @@ const ListGroup = ({
 
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-        }}
-      >
-        <Col xs={12}>
-          <div
-            style={{
-              marginLeft: "20px",
-              marginTop: "15px",
-              fontStyle: "normal",
-              fontWeight: "500",
-              fontSize: "24px",
-              lineHeight: "29px",
-              color: "#373737",
-            }}
-          >
-            {groupName}
-          </div>
-          {listData.slice(0, numberOfItemsToShow).map((object, index) => {
-            return (
-              <Row>
-                <ListItem
-                  profilePic={MMLogo}
-                  heading={
-                    resultType === "Gigs"
-                      ? object?.Name
-                      : resultType === "People"
-                      ? object?.FirstName + " " + object?.LastName
-                      : object?.Title
-                  }
-                  subheading={object.Skills}
-                  caption={
-                    resultType === "Gigs"
-                      ? object?.LocationName
-                      : resultType === "People"
-                      ? object?.Location?.city
-                      : object?.Description
-                  }
-                  // isBookmarked={true}
-                />
-              </Row>
-            );
-          })}
-
-          {isCompressedView && (
+      {listData.length > 0 && (
+        <div
+          style={{
+            display: "flex",
+          }}
+        >
+          <Col xs={12}>
             <div
               style={{
+                marginLeft: "20px",
                 marginTop: "15px",
                 fontStyle: "normal",
-                fontWeight: "600",
-                fontSize: "18px",
-                lineHeight: "22px",
-                color: "#9B9B9B",
-                textAlign: "center",
-                borderTop: "2px solid #CDE0FF",
-                paddingTop: "10px",
-                paddingBottom: "10px",
-                borderBottom: "4px solid #CDE0FF",
+                fontWeight: "500",
+                fontSize: "24px",
+                lineHeight: "29px",
+                color: "#373737",
               }}
-              onClick={onExpandResults}
             >
-              See all {groupName.toLowerCase()} results
+              {groupName}
             </div>
-          )}
-        </Col>
-      </div>
+            {listData.slice(0, numberOfItemsToShow).map((object, index) => {
+              return (
+                <Row>
+                  <ListItem
+                    profilePic={MMLogo}
+                    heading={
+                      resultType === "Gigs"
+                        ? object?.Name
+                        : resultType === "People"
+                        ? object?.FirstName + " " + object?.LastName
+                        : object?.Title
+                    }
+                    subheading={object.Skills}
+                    caption={
+                      resultType === "Gigs"
+                        ? object?.LocationName
+                        : resultType === "People"
+                        ? object?.Location?.city
+                        : object?.Description
+                    }
+                    // isBookmarked={true}
+                  />
+                </Row>
+              );
+            })}
+
+            {isCompressedView && (
+              <div
+                style={{
+                  marginTop: "15px",
+                  fontStyle: "normal",
+                  fontWeight: "600",
+                  fontSize: "18px",
+                  lineHeight: "22px",
+                  color: "#9B9B9B",
+                  textAlign: "center",
+                  borderTop: "2px solid #CDE0FF",
+                  paddingTop: "10px",
+                  paddingBottom: "10px",
+                  borderBottom: "4px solid #CDE0FF",
+                }}
+                onClick={onExpandResults}
+              >
+                See all {groupName.toLowerCase()} results
+              </div>
+            )}
+          </Col>
+        </div>
+      )}
     </>
   );
 };
--- frontend/src/app/components/SearchBar/index.jsx
+++ frontend/src/app/components/SearchBar/index.jsx
@@ -16,6 +16,7 @@ const SearchBar = ({ onSearchEvent }) => {
       <FaSearch className="search-icon" />
       <input
         type="text"
+        autoFocus
         placeholder="Search for..."
         onKeyUp={handleKeyUp}
         onClick={() => {
